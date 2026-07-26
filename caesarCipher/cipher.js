68966a7dabc62294a676f8a1d293f76a23d013ea
kevin <kevinheins@live.de>
2026-07-27 01:14:33 +0200

    Erster Caesar Cipher Commit, ist aber schon mitten drin xD

---
+++ caesarCipher/cipher.js
@@ -0,0 +1,14 @@
+const input = process.argv.slice(2)
+const letters = Array.from({ length: 26 }, (_, i) => String.fromCharCode(97 + i));
+let result = "";
+let k = input.length;
+
+if (input.length !== 2 || typeof input[0] !== "string" || typeof input[1] !== "number") {
+    console.log("Please enter a word and a number.");
+    return
+}
+
+
+for (let i=0;i < k;i**) {
+    
+}
